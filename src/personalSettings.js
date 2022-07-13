b759d2257c4b2828a66fc54febef93b8f9f2caa9
Julien Veyssier <eneiluj@posteo.net>
2022-07-13 15:05:26 +0200

    do not disable language settings
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- src/personalSettings.js
+++ src/personalSettings.js
@@ -4,10 +4,13 @@ export function disablePersonalSettingsFields() {
 	inputs.forEach((element) => {
 		element.setAttribute('readonly', 'readonly')
 	})
+	// do not disable language settings which are <select> tags
+	/*
 	const selects = document.querySelectorAll('#personal-settings select')
 	selects.forEach((element) => {
 		element.setAttribute('disabled', 'disabled')
 	})
+	*/
 	const textareas = document.querySelectorAll('#personal-settings textarea')
 	textareas.forEach((element) => {
 		element.setAttribute('readonly', 'readonly')
