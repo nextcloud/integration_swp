e55e792d3334664ded76d5ea561925843dfd50cc
Julien Veyssier <julien-nc@posteo.net>
2022-10-16 14:29:34 +0200

    disable profile checkbox in perso settings
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/personalSettings.js
+++ src/personalSettings.js
@@ -2,6 +2,9 @@ export function disablePersonalSettingsFields() {
 	// personal settings
 	const inputs = document.querySelectorAll('#personal-settings input')
 	inputs.forEach((element) => {
+		if (element.getAttribute('id') === 'enable-profile') {
+			element.setAttribute('disabled', 'disabled')
+		}
 		element.setAttribute('readonly', 'readonly')
 	})
 	// do not disable language settings which are <select> tags
