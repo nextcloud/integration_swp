132cb9e1281853f2df125db46d42391c5b181fcc
Andy Scherzinger <info@andy-scherzinger.de>
2024-11-29 16:52:06 +0100

    docs(reuse): Add SPDX header
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+<!--
+  - SPDX-FileCopyrightText: 2021 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
 # Nextcloud Sovereign Workplace (Phoenix) integration
 
 ## Implemented features
--- src/personalSettings.js
+++ src/personalSettings.js
@@ -1,3 +1,8 @@
+/**
+ * SPDX-FileCopyrightText: 2022 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 export function disablePersonalSettingsFields() {
 	// personal settings
 	const inputs = document.querySelectorAll('#personal-settings input')
